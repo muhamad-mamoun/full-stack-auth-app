1e38d4450911a9a5e4c8ee576dd8e57da15acdbc
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-03-13 18:02:09 +0200

    feat(backend): connect to the database cluster

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
+mongoose.connect(process.env.DATABASE_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to the database successfully [${mongoose.connection.host}]`);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('Failed to connect to the database');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}...`);
