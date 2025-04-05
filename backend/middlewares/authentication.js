5d247c234b03e73164170d43d8117873e3f6951c
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-04-05 20:27:22 +0200

    feat(backend): add cookie-parser to parse request cookies

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.cookie.split('=')[1];
-    const result = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        const token = req.cookies.Token;
 
-    if (!result) res.status(401).json({ statuxs: 'Refused', message: 'Unauthorized access', data: '' });
-    else {
-        req.userId = result.userId;
+        if (!token) {
+            return res.status(401).json({ status: 'Failed', message: 'Unauthorized: No token provided', data: '' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
         next();
+
+    } catch (error) {
+        res.status(401).json({ status: 'Failed', message: 'Unauthorized: Invalid or expired token', data: '' });
+        console.error(error);
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const router = require('./routes/index.route');
 
 dotenv.config();
@@ -10,6 +11,7 @@ const app = express();
 mongoose.connect(process.env.DATABASE_URI);
 
 app.use(cors({ origin: 'http://127.0.0.1', credentials: true }));
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/profile-picture', express.static('uploads'));
