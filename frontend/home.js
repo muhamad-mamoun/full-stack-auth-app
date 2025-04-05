f916c5c92063dbf22284354104a7a19cce9ce2bb
Muhamad Mamoun <muhamadmamoun@gmail.com>
2025-04-06 01:07:04 +0200

    feat(frontend): create home page template

---
+++ frontend/home.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const respose = await fetch('http://127.0.0.1:3000/api/v1/home/', {
+        method: 'GET',
+        credentials: 'include',
+    });
+
+    const body = await respose.json();
+    console.log(body);
+
+    document.querySelector('.user-name').textContent = body.data.FullName;
+    document.querySelector('.user-info').textContent = body.data.Email;
+    document.querySelector('.profile-picture>div').style.backgroundImage = `url(http://127.0.0.1:3000/profile-picture/${body.data.ProfilePicture})`;
+});
+
+document.getElementById('logout-button').addEventListener('click', async () => {
+    const respose = await fetch('http://127.0.0.1:3000/api/v1/auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+    });
+
+    const body = await respose.json();
+
+    if (body.status === 'Success') window.location.href = 'index.html';
+});
